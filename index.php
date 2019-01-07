ee7686fd19fb8f004786c03bd4bc3c5fee702c12
yury <zionlenuz1@gmail.com>
2019-01-07 17:00:01 -0500

    Front-End App Gallery

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php include './includes/__header.php'?>
+
+<div class="row">
+    <div id="" class="col-3 bg-primary vh-100 fixed-top pt-5">
+        <div class="container">
+            <img src="img/logo.png" alt="" class="img-fluid">
+            <div class="text-light text-center">
+                <a href="login.php" class="text-light mb-4">Log In</a> <br>
+                <a href="#" class="text-light">Log Out</a><br>
+                <a href="subir_foto.php" class="text-light">Subir Foto</a>
+                <p class="mt-5">“La fotografía es el único lenguaje que puede ser entendido y comprendido en todo el mundo”. Bruno Barbey”</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-9 ml-auto mt-5">
+        <div class="container">
+            <div class="card w-75">
+                <div class="card-header">
+                    <img src="img/runner.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="card-body">
+                "Detrás de la atelta que te has convertido, del entrenamiento de horas, de los coaches que te han impulsado está una pequeña niña que se enamoró del juego y nunca volteó para atras. Juega por ella", - Mia Hamm, jugadora profesional de futbol soccer (1987-2004)
+                </div>
+            </div>
+            <div class="alert alert-dark w-75">
+                Que la siguiente inspiracion sea el tuya ...
+            </div>
+            <div class="card w-75">
+                <div class="card-header">
+                    <img src="img/programmer.jpg" alt="" class="img-fluid">
+                </div>
+                <div class="card-body">
+                La mayoría de los buenos programadores programan, no porque esperan que se les pague o por adulación por parte del público, sino porque es divertido programar. Linus Torvalds
+                </div>
+            </div>
+            <div class="alert alert-dark w-75">
+                Que la siguiente inspiracion sea el tuya ...
+            </div>
+        </div>
+    </div>
+</div>
+<?php include './includes/__footer.php' ?>
---
+++ login.php
@@ -0,0 +1,23 @@
+<?php include './includes/__header.php'?>
+    <div class="d-flex justify-content-center mt-5">
+    <div class="card w-25">
+        <div class="card-header">
+            <img src="img/logo.png" alt="" class="w-100">
+        </div>
+        <div class="card-body">
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <input type="text" name="name" placeholder="insert a name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="user" placeholder="insert a username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" placeholder="insert a password" class="form-control">
+                </div>
+                <input type="submit" class="btn btn-primary btn-block">
+            </form>
+        </div>
+    </div>
+    </div>
+<?php include './includes/__footer.php' ?>
